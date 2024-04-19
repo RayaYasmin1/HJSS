001313a4b0f9db4e893a10e367199e69b93c60e6
RayaYasmin1 <rayayasmin111@gmail.com>
2024-04-19 21:12:49 +0100

    Last Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,13 @@ public class Main {
         boolean exitRequested = false;
         while (!exitRequested) {
             Scanner scanner = new Scanner(System.in);
+            System.out.println("\n");
             System.out.println("Welcome To The Hatfield Junior Swimming School (HJSS) Management System");
             System.out.println("1.Register");
             System.out.println("2.Book A Lesson");
             System.out.println("3.Coach Monthly report");
             System.out.println("4.Rate a lesson");
-            System.out.println("5.Change/Cancel A Booking");
+            System.out.println("5.Update/Cancel A Booking");
             System.out.println("6.Learner Monthly Report");
             System.out.println("7.Exit");
 
